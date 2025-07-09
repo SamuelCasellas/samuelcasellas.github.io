2e875572a642d7e4613633e9fb82272041521b62
Samuel Casellas <sam.casellas@gmail.com>
2025-07-09 15:15:26 -0400

    finished the minimalist implementation, pre UI/chrome extension. Accepts master password and website name

--- uniquePasswordManager.js
+++ uniquePasswordManager.js
@@ -1,28 +1,48 @@
 // SIMPLE FUNCTIONS
 
+const sum = (arr) => arr.reduce((acc, curr) => acc + curr, 0);
+
 var seedrandom = require('seedrandom');
 
 const charset = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-function randomizeKeyFromMasterPasswordBasedOnWebsite(masterPassword, website) {
-  
+function scrambleKeyFromMasterPasswordBasedOnWebsite(masterPassword, website) {
+  let key = '';
+  const charValues = Array.from(website).map(c => c.charCodeAt(0));
+  const totalCharValue = sum(charValues);
+  const passwordLen = masterPassword.length;
+  let index = totalCharValue % passwordLen;
+  let jumpAmount = sum(charValues.slice(4, 8));
+  let thisSum = 0;
+  for (let i = 0; i < 32; i++) {
+    let [a, b, c] = [masterPassword.at(index), masterPassword.at(index - 1), masterPassword.at(index - 2)];
+    thisSum += a.charCodeAt(0) + b.charCodeAt(0);
+    key += String.fromCharCode((thisSum % 125) + 33);
+    index = (index + jumpAmount) % passwordLen;
+    jumpAmount += c.charCodeAt(0) % 3;
+  }
+  console.log('my key!', key);
+  return key;
 }
 
 
-function createHash(createHmac, secret) {
-  const hash = createHmac('sha256', secret).digest('hex');
-  return hash.slice(17, 49);
-}
+/**
+ * Return a portion of the 64 characters in length
+ * @param {*} createHmacFunc 
+ * @param {*} secret 
+ * @returns 
+ */
+const createHash = (createHmacFunc, key) => createHmacFunc('sha256', key).digest('hex');
 
 /**
  * Generate salt that is 32 characters long using random seed based on hashed master key
  * Recommended to be at least 16 characters long
- * @param {*} createHmac 
- * @param {*} masterPassword 
+ * @param {*} createHmacFunc 
+ * @param {*} key 
  */
-function generateSalt(createHmac, masterPassword) {
+function generateSalt(createHmacFunc, key) {
   // Set the seed for the random number generator
-  var rng = seedrandom(createHash(createHmac, masterPassword.substring(6)).substring(10, 25));
+  var rng = seedrandom(createHash(createHmacFunc, key));
   let salt = '';
   for (let i = 0; i < 32; i++) {
     salt += charset[Math.floor(rng() * charset.length)];
@@ -40,15 +60,17 @@ function generateSalt(createHmac, masterPassword) {
  * @param {*} length password length
  * @returns 
  */
-async function generateUniquePassword(masterPassword, website, length = 16, iterations = 100000) {
+async function generateUniquePassword(masterPassword, website, length = 25, iterations = 100000) {
+  // Not running from a server so sync version is fine
   const { createHmac, pbkdf2Sync } = await import('node:crypto');
 
   // Apply a key derivation function (e.g., HMAC-SHA256)
 
   // Implementing pbkdf2 with all its parameters
   // Use PBKDF2 to derive the password
-  const salt = generateSalt(createHmac, website);
-  console.log(salt);
+  const key = scrambleKeyFromMasterPasswordBasedOnWebsite(masterPassword, website);
+  const salt = generateSalt(createHmac, key);
+  console.log('salt', salt);
 
   return pbkdf2Sync(
     masterPassword,
@@ -56,7 +78,7 @@ async function generateUniquePassword(masterPassword, website, length = 16, iter
     iterations,
     length,
     'sha512')
-    .toString('hex')
+    .toString('ascii')
     /* 
     "ascii"
             | "utf8"
@@ -78,14 +100,14 @@ async function generateUniquePassword(masterPassword, website, length = 16, iter
 
 function main() {
   (async () => {
-    const masterPassword = 'Pancreatic cancer'
-    const website = 'twitter'
+    const masterPassword = 'what is the meaning of this'
+    const website = 'n.com'
     console.log(await generateFromMasterPassword(masterPassword, website));
   })();
 }
 
 async function generateFromMasterPassword(masterPassword, website) {
-  await generateUniquePassword(masterPassword, website);
+  return await generateUniquePassword(masterPassword, website);
 }
 
 main();
